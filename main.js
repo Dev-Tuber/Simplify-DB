7d91029ca0403cca30f8e237a6ae018bbbc68424
Dev-Tuber <142738540+Dev-Tuber@users.noreply.github.com>
2023-10-06 16:28:52 +0900

    System All Reset

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-const database = require('./database');
+const crypto = require('crypto');
+const fs = require('fs');
 
-// 데이터 불러오기
-const data = database.loadData();
-console.log('Loaded Data:', data);
+// 비밀키와 초기 벡터 생성
+const secretKey = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
-// 새 데이터 추가
-const newData = { id: 1, name: 'My Name', age: 1000 };
-data.push(newData);
+// 데이터 준비
+let dataArr = [
+    {"name": "John", "age": 30, "city": "New York"},
+    {"name": "Jane", "age": 25, "city": "Los Angeles"},
+    {"name": "John", "age": 30, "city": "New York"},
+    {"name": "Jane", "age": 25, "city": "Los Angeles"},
+    // 추가적인 데이터...
+];
 
-// 변경된 데이터 저장
-database.saveData(data);
-console.log('Data Saved:', data);
+// 각각의 데이터를 암호화하고 배열에 추가
+let encryptedDataArr = dataArr.map(data => {
+    let cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
+    let encryptedData = cipher.update(JSON.stringify(data), 'utf8', 'hex');
+    encryptedData += cipher.final('hex');
+    
+    return encryptedData;
+});
+
+// 암호화된 데이터 목록을 JSON 형식으로 변환하여 파일에 저장
+fs.writeFileSync('db.json', JSON.stringify(encryptedDataArr));
+
+console.log('Loaded Data:', dataArr);
